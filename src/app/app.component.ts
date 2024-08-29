fecb67c60cdfcd4368e5a2f20c0d45be61fcf2f5
IngEnigma <zs220199@estudiantes.uv.mx>
2024-08-29 17:35:40 -0600

    add work_experience branch #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
