a1edf2cbcc653e9df0f5aaf506b3c3c144e097eb
IngEnigma <zs220199@estudiantes.uv.mx>
2024-08-31 18:05:41 -0600

    add education branch #3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { EducationComponent } from './education/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    WorkExperienceComponent,
+    EducationComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'MyCv';
