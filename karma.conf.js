5c981c6f72ee1c81d121dc5cd8034a793feeab11
IngEnigma <159200281+IngEnigma@users.noreply.github.com>
2024-09-26 17:11:04 -0600

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
     },
     reporters: ['progress', 'kjhtml'],
     browsers: ['Chrome'],
-    browsers:['ChromeHeadless'],
+    browsers:['ChromeHeadlessCI'],
      customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
